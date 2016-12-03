3a0f83bb37a506212dd1149bb2451f6a1c40c15a
javaben <javaben@gmail.com>
2016-12-03 19:07:08 +0800

    Update Subscription.js

--- Subscription.js
+++ Subscription.js
@@ -83,9 +83,6 @@ function postSubscribeObj(endpoint, key, secret) {
 
     var database = firebase.database();
     
-    var user = firebase.auth().currentUser;
-    console.log(user);
-    
     firebase.database().ref('users/'+ user).set({
             Endpoint: endpoint,
             PublicKey: btoa(String.fromCharCode.apply(null, new Uint8Array(key))),
