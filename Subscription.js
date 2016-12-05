2cf3732024d362d76e27aa15a8833eb4337ad7f7
javaben <javaben@gmail.com>
2016-12-05 22:40:59 +0800

    Update Subscription.js

--- Subscription.js
+++ Subscription.js
@@ -21,10 +21,7 @@ window.addEventListener('load', function () {
         // The signed-in user info.
         user = result.user;
         console.log(user);
-        
-        //calendar test
-        AddEvent();
-            
+       
         requestPermession();
         // ...
       }).catch(function(error) {
@@ -39,38 +36,7 @@ window.addEventListener('load', function () {
       });
    
 });
-
-function AddEvent(){
-var event = {
-  'summary': 'Google I/O 2015',
-  'location': '800 Howard St., San Francisco, CA 94103',
-  'description': 'A chance to hear more about Google\'s developer products.',
-  'start': {
-    'dateTime': '2017-01-01T09:00:00-07:00',
-    'timeZone': 'America/Los_Angeles'
-  },
-  'end': {
-    'dateTime': '2017-01-02T17:00:00-07:00',
-    'timeZone': 'America/Los_Angeles'
-  },
-  'recurrence': [
-    'RRULE:FREQ=DAILY;COUNT=2'
-  ],
-  'attendees': [
-    {'email': 'lpage@example.com'},
-    {'email': 'sbrin@example.com'}
-  ],
-  'reminders': {
-    'useDefault': false,
-    'overrides': [
-      {'method': 'email', 'minutes': 24 * 60},
-      {'method': 'popup', 'minutes': 10}
-    ]
-  }
-};
-
-
-      
+ 
 var request = gapi.client.calendar.events.insert({
   'calendarId': 'primary',
   'resource': event
